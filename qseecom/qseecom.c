4569b5fd526ddf8c44c19092e24c55e9cdc78ac3
Monika Singh <monising@codeaurora.org>
2021-02-26 23:47:09 +0530

    qseecom: Add flush_work based on flag
    As INIT_WORK during suspend resume path is
    only supported with bus scaling support
    hence use the same check for flush_work.
    
    Change-Id: I5172d574a2bc33a1cdba1fead7e6c4208648448c
    Signed-off-by: Mon(CR) Singh <monising@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/2727378
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- qseecom/qseecom.c
+++ qseecom/qseecom.c
@@ -10053,7 +10053,8 @@ static int qseecom_suspend(struct device *dev)
 
 	mutex_unlock(&clk_access_lock);
 	mutex_unlock(&qsee_bw_mutex);
-	cancel_work_sync(&qseecom.bw_inactive_req_ws);
+	if (qseecom.support_bus_scaling)
+		cancel_work_sync(&qseecom.bw_inactive_req_ws);
 
 	return 0;
 }
